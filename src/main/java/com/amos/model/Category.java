552f732abffde38ec08e11fbfd36d98cbb37a390
shamos13 <akwatuha04@gmail.com>
2025-11-04 14:35:08 +0300

    Completed the FoodCategory  Entity

--- src/main/java/com/amos/model/Category.java
+++ src/main/java/com/amos/model/Category.java
@@ -1,9 +1,7 @@
 package com.amos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,4 +14,11 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+
 }
