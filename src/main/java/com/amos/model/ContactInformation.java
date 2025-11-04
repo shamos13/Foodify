8d05ef911ff8b7d8f8ccfea2f1deda20a65f40bb
shamos13 <akwatuha04@gmail.com>
2025-11-04 14:25:10 +0300

    Completed the Restaurant Entity

---
+++ src/main/java/com/amos/model/ContactInformation.java
@@ -0,0 +1,11 @@
+package com.amos.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    private String email;
+    private String phone;
+    private String twitter;
+    private String instagram;
+}
