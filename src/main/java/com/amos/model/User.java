ae2c07401d3997688a3c7e33b94ece3fece58631
shamos13 <akwatuha04@gmail.com>
2025-11-04 12:29:19 +0300

    First Commit of Foodify

---
+++ src/main/java/com/amos/model/User.java
@@ -0,0 +1,29 @@
+package com.amos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+
+    private USER_ROLE role;
+
+
+
+
+}
